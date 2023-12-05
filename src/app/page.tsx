f4843fd308d28d80580e6706c432bbc4f63ab849
Hossein Rahimi <hrahimi270@gmail.com>
2023-12-05 19:02:56 +0330

    feat(reservations): fetching reservations of month and send it to the calendar wrapper component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,41 @@
 import Link from "next/link";
 
 import { getServerAuthSession } from "@/server/auth";
+import CalendarWrapper from "@/components/CalendarWrapper";
+import { api } from "@/trpc/server";
 
-export default async function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { year?: string; month?: string };
+}) {
   const session = await getServerAuthSession();
+
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth() + 1;
+
+  const monthReservations = session?.user
+    ? await api.reservation.getMonthReservations.query({
+        year: Number(searchParams.year) || currentYear,
+        month: Number(searchParams.month) || currentMonth,
+      })
+    : [];
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="flex flex-col items-center gap-2">
         <div className="flex flex-col items-center justify-center gap-4">
           <p className="text-center text-2xl">
-            {session && <span>Logged in as {session.user?.name}</span>}
+            <span>Logged in as {session?.user?.name}</span>
           </p>
           <Link
-            href={session ? "/api/auth/signout" : "/api/auth/signin"}
+            href="/api/auth/signout"
             className="rounded-full bg-slate-200 px-10 py-3 font-semibold no-underline transition hover:bg-slate-400"
           >
-            {session ? "Sign out" : "Sign in"}
+            Sign out
           </Link>
+          <CalendarWrapper monthReservations={monthReservations} />
         </div>
       </div>
     </main>
